5ba0d11e9144cc6635ec4dec1642c9703e091ddd
unknown <ebenezermanka1234@gmail.com>
2023-09-25 10:27:24 +0530

    original ext

--- ll.py
+++ ll.py
@@ -41,3 +41,57 @@ ll.append(3)
 ll.append(2)
 ll.prepend(10)
 ll.print_val()
+
+# Introduction To Linked List
+def constructLL(arr: [int]) -> Node:
+    head=Node(arr[0])
+    temp=head
+    for i in range(1,len(arr)):
+        curr=Node(arr[i])
+        temp.next=curr
+        temp=temp.next
+    return head
+
+# Insert Node At The Beginning
+def insertAtFirst(list: Node, newValue: int) -> Node:
+    # Write your code here
+    new=Node(newValue)
+    new.next=list
+    return new
+
+# 237. Delete Node in a Linked List(Leetcode)
+def deleteNode(self, node):
+    """
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
+    node.val=node.next.val
+    node.next=node.next.next
+
+# Delete Node Of Linked List
+def deleteLast(list: Node) -> Node:
+    # Write your code here
+    temp=list
+    while list.next.next!=None:
+        list=list.next
+    list.next=None
+    return temp
+
+# Count nodes of linked list
+def length(head) :
+    c=0
+    while head!=None:
+        c+=1
+        head=head.next
+    return c
+
+# Search in a Linked List
+def searchInLinkedList(head, k):
+    # Your code goes here.
+    while head!=None:
+        if k==head.data:
+            return 1
+        head=head.next
+    return 0
+
+
